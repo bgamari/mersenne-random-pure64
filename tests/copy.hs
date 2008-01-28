2a990f23b70ff41573493bea3e7e783f665fc6f5
Don Stewart <dons@galois.com>
2008-01-28 02:08:53 +0000

    update copy test

--- tests/copy.hs
+++ tests/copy.hs
@@ -1,12 +1,15 @@
-import System.Random.Mersenne.Pure
+{-# LANGUAGE BangPatterns #-}
+
+import System.Random.Mersenne.Pure64
 
 main = do
 
-    g <- init_genrand64 7
+    let g = pureMT 7
 
     let go :: Int -> PureMT -> IO ()
-        go 0 _ = return ()
-        go n p = do q <- copyPureMT p
-                    go (n-1) q
+        go 0 !p = return ()
+        go n !p  = do
+            let (_,q) = randomWord p
+            go (n-1) q
 
     go 10000000 g -- 10s constant space
